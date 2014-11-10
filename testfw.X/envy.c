c117a62f2e065c43b813257b6e10049b0699470f
wira <wira@wintermute>
2014-11-09 20:09:34 -0600

    added gpl3 headers and COPYING file. Added .c and .h files for envy (NVM loader)

--- testfw.X/envy.c
+++ testfw.X/envy.c
@@ -16,27 +16,4 @@
 
  */
 
-/* 
- * File:   plpemu.h
- * Author: Wira
- *
- * Created on November 4, 2014, 1:09 PM
- */
-
-#ifndef PLPEMU_H
-#define	PLPEMU_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-    void emu_plp5(int);
-    void fload(void);
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* PLPEMU_H */
-
+// JERUK Flash loader routines
--- testfw.X/envy.h
+++ testfw.X/envy.h
@@ -17,43 +17,25 @@
  */
 
 /* 
- * File:   wio.h
+ * File:   envy.h
  * Author: Wira
  *
- * Created on October 30, 2014, 12:31 PM
+ * Created on November 9, 2014, 8:08 PM
  */
 
-#ifndef WIO_H
-#define	WIO_H
+#ifndef ENVY_H
+#define	ENVY_H
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-void init_uart1(int, int);
-void init_uart2(int, int);
-void print(char*);
-void psstr(char*, int, int);
-void pchar(char);
-char blocking_read(void);
-char str_cmp(char*, char*, int);
-char parse_ascii_hex_byte(char*, int);
-int parse_ascii_hex_32(char*, int);
-char parse_ascii_hex_bit(char*, int);
-char ascii_byte_h(char);
-char ascii_byte_l(char);
-char parse_ascii_hex(char);
-char ascii_nybble(char);
-void ascii_hex_word(char[], int);
-void uart_forward(void);
-void u2_set_baud(int);
-char u2_blocking_read(void);
-void u2_read_print(void);
-void u2_write(char);
+
+
 
 #ifdef	__cplusplus
 }
 #endif
 
-#endif	/* WTERMIO_H */
+#endif	/* ENVY_H */
 
